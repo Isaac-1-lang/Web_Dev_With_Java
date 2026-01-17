ef3220c27488cb5e6c3a1d88d92aa6a6084d04a1
NIYOBYOSE-Isaac-Precieux <isaprecieux112@gmail.com>
2026-01-17 11:41:42 +0200

    Phone number validator

--- src/main/java/com/helloworld/util/PhoneNumberValidator.java
+++ src/main/java/com/helloworld/util/PhoneNumberValidator.java
@@ -1,4 +1,15 @@
 package com.helloworld.util;
 
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 public class PhoneNumberValidator {
+    private String PHONE_NUMBER_VALIDATOR="[0-9]{10}";
+
+
+    public boolean validatePhoneNumber(String phoneNumber) {
+        Pattern pattern = Pattern.compile(PHONE_NUMBER_VALIDATOR);
+        Matcher matcher = pattern.matcher(phoneNumber);
+        return matcher.matches();
+    }
 }
