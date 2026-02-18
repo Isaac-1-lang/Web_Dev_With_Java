6d7e97b5dac7068eb3e4574dbbdb75fba248755f
NIYOBYOSE-Isaac-Precieux <isaprecieux112@gmail.com>
2026-02-18 07:41:30 +0200

    Added a checking flag

---
+++ src/main/java/com/helloworld/Check.java
@@ -0,0 +1,12 @@
+package com.helloworld;
+
+public class Check {
+
+
+    static int count=0;
+    public static void main(String[] args) {
+        System.out.println(++count);
+        System.out.println(count);
+
+    }
+}
